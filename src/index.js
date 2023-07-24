fba2989c122111505da2e294d4bacefa00eb9f62
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-24 13:19:46 -0400

    Create test projects and display them

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+import Project from "./logic/project";
+import Projects from "./logic/projects";
 import "./styles/main.scss";
 import { loadHeader } from "./ui/header";
+import { loadProjectsTab } from "./ui/projectsTab";
 
 function loadPage() {
   loadHeader();
+  Projects.addProject(new Project("test"));
+  Projects.addProject(new Project("test"));
+  Projects.addProject(new Project("test"));
+  Projects.addProject(new Project("test"));
+  Projects.addProject(new Project("test"));
+  loadProjectsTab(Projects.allProjects);
 }
 
 loadPage();
