ec0c510f3ae1e65cb677c89db0e66b51c9740af2
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-24 13:31:31 -0400

    Add a button to create new projects

--- src/ui/projectsTab.js
+++ src/ui/projectsTab.js
@@ -1,11 +1,13 @@
 function createProjectsTab(projects) {
+  const projectsContainer = document.createElement("div");
+  projectsContainer.classList.add("projects-container");
   const projectsDiv = document.createElement("div");
   projectsDiv.classList.add("projects");
 
   const projectsDivTitle = document.createElement("h2");
   projectsDivTitle.innerText = "Projects";
   projectsDivTitle.classList.add("projects-title");
-  projectsDiv.appendChild(projectsDivTitle);
+  projectsContainer.appendChild(projectsDivTitle);
 
   for (const project of projects) {
     const projectDiv = document.createElement("div");
@@ -15,7 +17,15 @@ function createProjectsTab(projects) {
     projectDiv.appendChild(projectName);
     projectsDiv.appendChild(projectDiv);
   }
-  return projectsDiv;
+  projectsContainer.appendChild(projectsDiv);
+
+  // Add projects button
+  const addButton = document.createElement("button");
+  addButton.innerText = "Create new project";
+  addButton.classList.add("projects-button");
+  projectsContainer.appendChild(addButton);
+
+  return projectsContainer;
 }
 
 export function loadProjectsTab(projects) {
