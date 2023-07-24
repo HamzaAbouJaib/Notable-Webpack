56bcc0d1d4f16cf535aa3c58e1f7e40fd047fffd
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-24 19:39:52 -0400

    Create a modal that takes in content to display in the modal

---
+++ src/ui/modal.js
@@ -0,0 +1,27 @@
+function createModal(content) {
+  const background = document.createElement("div");
+  background.classList.add("modal-background");
+  background.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal-background")) {
+      removeModal();
+    }
+  });
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  background.appendChild(modal);
+  modal.appendChild(content);
+  return background;
+}
+
+export function loadModal(modalContent) {
+  const content = document.querySelector("#content");
+  content.appendChild(createModal(modalContent));
+}
+
+function removeModal() {
+  const modal = document.querySelector(".modal-background");
+  const content = document.querySelector("#content");
+  if (modal) {
+    content.removeChild(modal);
+  }
+}
