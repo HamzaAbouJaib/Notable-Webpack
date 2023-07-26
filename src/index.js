56e6a7a1bffa6422da4dd0bf3be9915e88b9aaac
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-25 21:47:02 -0400

    Create the tab container

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import Project from "./logic/project";
 import Projects from "./logic/projects";
 import "./styles/main.scss";
+import { loadTodoTab } from "./ui/TodoTab";
 import { loadHeader } from "./ui/header";
 import { loadProjectsTab } from "./ui/projectsTab";
 
 function loadPage() {
   loadHeader();
+  createTabContainer();
   loadProjectsTab(Projects.allProjects);
+  loadTodoTab();
+}
+
+function createTabContainer() {
+  const tabContainer = document.createElement("div");
+  tabContainer.classList.add("tab-container");
+  const content = document.querySelector("#content");
+  content.appendChild(tabContainer);
 }
 
 loadPage();
