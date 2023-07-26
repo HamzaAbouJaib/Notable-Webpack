5d17561c5714da247920b8fd5486412ed6ece0bd
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-26 17:22:30 -0400

    Display todo information

--- src/ui/todos.js
+++ src/ui/todos.js
@@ -8,14 +8,25 @@ export function createTodos(todos) {
     const todoTitle = document.createElement("h4");
     todoTitle.innerText = todo.title;
     todoDiv.appendChild(todoTitle);
+    const datePriorityContainer = document.createElement("div");
+    const todoDate = document.createElement("p");
+    todoDate.innerText = todo.date;
+    datePriorityContainer.appendChild(todoDate);
+    const seperator = document.createElement("p");
+    seperator.innerText = "-";
+    datePriorityContainer.appendChild(seperator);
+    const todoPriority = document.createElement("p");
+    todoPriority.innerText = todo.priority;
+    datePriorityContainer.appendChild(todoPriority);
+    todoDiv.appendChild(datePriorityContainer);
+    const todoDesc = document.createElement("p");
+    todoDesc.innerText =
+      todo.description.length > 100
+        ? todo.description.slice(0, 100) + "..."
+        : todo.description;
+    todoDiv.appendChild(todoDesc);
     todosContainer.appendChild(todoDiv);
   }
 
   return todosContainer;
-}
-
-function clearTodos() {
-  const content = document.querySelector(".project-container");
-  const todos = document.querySelector(".todos-container");
-  if (content && tabs) content.removeChild(tabs);
 }
