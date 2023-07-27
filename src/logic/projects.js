f52548eead9cfb96150f302bd404a10911c93a1c
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-27 13:22:08 -0400

    Use local storage to store projects and their todos

--- src/logic/projects.js
+++ src/logic/projects.js
@@ -1,16 +1,41 @@
+import Project from "./project";
+
 class Projects {
   static allProjects = [];
 
+  static initializeProjects() {
+    Projects.allProjects =
+      JSON.parse(localStorage.getItem("allProjects")) || [];
+  }
+
   static getProjects() {
+    const projects = JSON.parse(localStorage.getItem("allProjects"));
+    const p2 = [];
+    for (const project of projects) {
+      p2.push(new Project(project.name, project.todos, project.id));
+    }
+    Projects.allProjects = p2;
     return Projects.allProjects;
   }
 
+  static updateProjects(project) {
+    Projects.allProjects = Projects.getProjects().map((p) => {
+      if (project.id !== p.id) return p;
+      return project;
+    });
+    localStorage.setItem("allProjects", JSON.stringify(Projects.allProjects));
+  }
+
   static addProject(project) {
-    return Projects.allProjects.push(project);
+    Projects.allProjects = Projects.getProjects().push(project);
+    localStorage.setItem("allProjects", JSON.stringify(Projects.allProjects));
   }
 
   static removeProject(project) {
-    return Projects.allProjects.filter((p) => project.id !== p.id);
+    Projects.allProjects = Projects.getProjects().filter(
+      (p) => project.id !== p.id
+    );
+    localStorage.setItem("allProjects", JSON.stringify(Projects.allProjects));
   }
 }
 
