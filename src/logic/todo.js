6a9a2c07c98198ab52665ecd287f1c41c6092c6c
HamzaAbouJaib <hamza.aboujaib.0303@gmail.com>
2023-07-28 20:00:16 -0400

    Add ability to pass an existing id

--- src/logic/todo.js
+++ src/logic/todo.js
@@ -1,13 +1,13 @@
 import { v4 as uuidv4 } from "uuid";
 
 class Todo {
-  constructor(title, description, date, priority) {
+  constructor(title, description, date, priority, id) {
     this.title = title;
     this.description = description;
     this.date = date;
     this.priority = priority;
     this.isComplete = false;
-    this.id = uuidv4();
+    this.id = id || uuidv4();
   }
 }
 
